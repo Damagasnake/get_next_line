1bda0afb84b1a4e6cf7b2630d74229c5b2baad21
Damaga <damagasnake@gmail.com>
2024-10-21 10:33:53 +0200

    structure

---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 10:13:20 by davidma2          #+#    #+#             */
+/*   Updated: 2024/10/21 10:24:12 by davidma2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#include <unistd.h>
+size_t	ft_strlen(const char *str);
+#endif
