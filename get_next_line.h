69ac02b7fe1f025db87ce9aa61670056111c22e5
Damaga <damagasnake@gmail.com>
2024-10-22 15:56:57 +0200

    get_next_line final day 1

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:13:20 by davidma2          #+#    #+#             */
-/*   Updated: 2024/10/22 14:57:57 by davidma2         ###   ########.fr       */
+/*   Updated: 2024/10/22 15:42:52 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
 # endif
-#include <unistd.h>
+# include <unistd.h>
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
 #endif
