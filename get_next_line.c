74730fa8229a2aa13aff86259948d5c1ffecc5ae
David Martinez Gallego <davidma2@c2r12s4.42madrid.com>
2024-10-22 11:18:04 +0200

    Current progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,36 @@
 /*   By: davidma2 <davidma2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:11:35 by davidma2          #+#    #+#             */
-/*   Updated: 2024/10/21 12:06:40 by davidma2         ###   ########.fr       */
+/*   Updated: 2024/10/22 11:17:54 by davidma2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	free_space(char **res, int index)
+static void	free_space(char **res, int index)
 {
 	while (index >= 0)
 		free(res[index--]);
 	free(res);
+}
+
+ssize_t	read(int fd, void buf[.count], size_t count)
+{
+	ssize_t	i;
+
+	i = 0;
+}
+
+char	*get_next_line(int fd)
+{
+	char	*res;
+	char	buffer[BUFFER_SIZE];
+	ssize_t	bytesr;
+
+#define BUFFER_SIZE 42
+	index = 0;
+	bytesr = read(fd, buffer, BUFFER_SIZE);
+	res = (char *)malloc((bytesr + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
 }
