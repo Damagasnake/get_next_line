6339af0c5ec6c119df8d05ed0a2a9f93cff8c978
Damaga <damagasnake@gmail.com>
2024-10-29 13:55:07 +0100

    Finish?

--- get_next_line.c
+++ get_next_line.c
@@ -44,11 +44,10 @@ static char *returnline(char *buf)
 	char *newline;
 	size_t i;
 	size_t j;
-	
+
 	i = 0;
 	if (!buf)
 		return(NULL);
-	while (buf[i] && buf[i] != '\n')
 		i++;
 	if (buf[i] == '\n')
 		newline = (char *)malloc((i + 2) * sizeof(char));
@@ -60,6 +59,7 @@ static char *returnline(char *buf)
 	while (j < i)
 	{
 		newline[j] = buf[j];
+	while (buf[i] && buf[i] != '\n')
 		j++;
 	}
 	if (buf[i] == '\n')
@@ -75,7 +75,7 @@ static char *nextbuff(char *buf)
 	char *secondBuffer;
 	size_t i;
 	size_t j;
-	
+
 	i = 0;
 	while (buf[i] && buf[i] != '\n')
 		i++;
@@ -97,4 +97,26 @@ static char *nextbuff(char *buf)
 	secondBuffer[j] = '\0';
 	free(buf);
 	return(secondBuffer);
+}
+char *get_next_line(int fd)
+{
+	static char *buffer;
+	char *newline;
+	char temp_buffer[BUFFER_SIZE + 1];
+	ssize_t bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while ((bytes_read = read(fd, temp_buffer, BUFFER_SIZE)) > 0)
+	{
+		temp_buffer[bytes_read] = '\0';
+		buffer = ft_strjoin(buffer, temp_buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	if (bytes_read < 0 || (bytes_read == 0 && (!buffer || !*buffer)))
+		return (NULL);
+	newline = returnline(buffer);
+	buffer = nextbuff(buffer);
+	return (newline);
 }
