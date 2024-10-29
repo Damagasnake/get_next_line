247f4f2fad15b61f63bb9710cd4f59dab34c736c
Damaga <damagasnake@gmail.com>
2024-10-29 12:00:20 +0100

    b4 break

--- get_next_line.h
+++ get_next_line.h
@@ -17,6 +17,7 @@
 #  define BUFFER_SIZE 42
 # endif
 # include <unistd.h>
+# include <stdlib.h>
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
 //static void	free_space(char *res, int index)
